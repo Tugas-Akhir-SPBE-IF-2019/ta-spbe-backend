e4ee2077f163110fafca0a6da9ff3422d11fb5b0
akramab <13519142@std.stei.itb.ac.id>
2023-01-25 07:35:55 +0700

    feat: add database connection

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"ta-spbe-backend/api/handlers"
@@ -9,22 +10,33 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	assessmentService := services.NewAssessmentService()
 	assessmentHandler := handlers.NewAssessmentHandler(assessmentService)
 
+	connStr := "postgresql://ta-spbe:ta-spbe@db/ta-spbe?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Route("/assessment", func(r chi.Router){
+	r.Route("/assessment", func(r chi.Router) {
 		r.Mount("/", routers.AssessmentRouter(assessmentHandler))
 	})
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Tugas Akhir Otomatisasi Penilaian Tingkat Kematangan Kebijakan SPBE IF 2019"))
 	})
-
-
 
 	port := 80
 	log.Printf("Server started on port:%d!", port)
